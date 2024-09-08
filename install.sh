400ea1d42920c57c07dff87bf54bd9d04534661a
SkyNemesis <88611212+Stevens0856@users.noreply.github.com>
2024-09-08 18:57:02 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -93,7 +93,7 @@ install_theme() {
     read -r SELECT_THEME
     case "$SELECT_THEME" in
       1)
-        THEME_URL=$(echo -e "https://github.com/Stevens0856/ThemaInstaller/raw/C2.zip")
+        THEME_URL=$(echo -e "https://github.com/Stevens0856/ThemaInstaller/main/raw/C2.zip")
         break
         ;;
       2)
@@ -101,7 +101,7 @@ install_theme() {
         break
         ;;
       3)
-        THEME_URL=$(echo -e "https://github.com/Stevens0856/ThemaInstaller/raw/C3.zip")
+        THEME_URL=$(echo -e "https://github.com/Stevens0856/ThemaInstaller/main/raw/C3.zip")
         break
         ;; 
       x)
